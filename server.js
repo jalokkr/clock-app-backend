42dda99ffc5390dc746c19b06e01616355f649d8
alok kumar jha <alok@emertech.io>
2024-07-22 17:40:52 +0530

    merging app.js and index.js to -> server.js for better server configuration

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import usersRoutes from "./src/components/routes/users.js";
+import productRoutes from "./src/components/routes/products.js";
+import ordersRoutes from "./src/components/routes/orders.js";
 
 // Load environment variables from a .env file
 dotenv.config();
@@ -14,7 +17,8 @@ app.use(bodyParser.json());
 // Connection to MongoDB
 const mongoUri = process.env.DB_HOST;
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -22,8 +26,18 @@ mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     console.error("Failed to connect to MongoDB", err);
   });
 
+// Basic route for testing
+app.get("/", (req, res) => {
+  res.send("Hello World! The API is working.");
+});
+
+// Define your routes
+app.use("/users", usersRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", ordersRoutes);
+
 // Start the Express server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
