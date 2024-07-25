943a9e5fea6c78306cfab1a842f1acedbb32311f
alok kumar jha <alok@emertech.io>
2024-07-25 07:22:58 +0530

    remove: removed console logs from check role middleware

--- src/components/middlewares/roleMiddleware.js
+++ src/components/middlewares/roleMiddleware.js
@@ -1,7 +1,5 @@
 export const checkRole = (roles) => {
   return (req, res, next) => {
-    console.log(req)
-    console.log(req.user)
     if (!roles.includes(req.user.role)) {
       return res
         .status(403)
